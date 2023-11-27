46698a6a7bd9251da8c248e295dc3fd1654a15a1
Nikita <np.shche@gmail.com>
2023-11-27 16:15:30 +0300

    refactor highlighting a tile

--- Assets/Scripts/InteractTile.cs
+++ Assets/Scripts/InteractTile.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class InteractTile : MonoBehaviour
@@ -11,15 +12,44 @@ public class InteractTile : MonoBehaviour
    [SerializeField]
    private Color _color;
 
+   [SerializeField]
+   private Material _defaultMaterial;
+   
+   private Renderer _currentRenderer;
+   private Renderer _previousRenderer;
+
    private void Update()
    {
       var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
       if (Physics.Raycast(ray, out var hitInfo, 100f, _layer))
       {
-         var hitObject = hitInfo.collider.gameObject;
-
-         hitObject.GetComponent<Renderer>().material.color = _color;
+         _currentRenderer = hitInfo.collider.gameObject.GetComponent<Renderer>();
+         
+         if (_currentRenderer != _previousRenderer || _previousRenderer == null)
+         {
+            TryResetHighlight();
+            HighlightTile(hitInfo);
+         }
+      }
+      else
+      {
+        TryResetHighlight();
       }
    }
+
+   private void TryResetHighlight()
+   {
+      if (_previousRenderer != null)
+      {
+         _previousRenderer.material.color = _defaultMaterial.color;
+      }
+   }
+
+   private void HighlightTile(RaycastHit hitInfo)
+   {
+      _currentRenderer.material.color = _color;
+      _previousRenderer = _currentRenderer;
+   }
 }
+
